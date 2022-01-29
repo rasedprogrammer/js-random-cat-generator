fb5067f0bd9a1fec21523ad43880781f83f34efb
rasedprogrammer <rasedprogrammer@gmail.com>
2022-01-29 22:56:50 +0600

    uploading full project called random cat generator with api

---
+++ script.js
@@ -0,0 +1,7 @@
+function generatCat() {
+	var image = document.createElement("img");
+	var div = document.getElementById("flex-cat-gen");
+	image.src =
+		"http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+	div.appendChild(image);
+}
